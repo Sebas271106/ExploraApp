4e405e3cb9fd2563d7abffc441ce6315f08b6ca3
Sebas271106 <juansebastiansuarezpava@gmail.com>
2026-04-10 09:36:30 -0500

    Configuracion De La Navegacion

--- app/src/main/java/com/juansuarez/exploraapp/auth/LoginScreen.kt
+++ app/src/main/java/com/juansuarez/exploraapp/auth/LoginScreen.kt
@@ -3,9 +3,11 @@ package com.juansuarez.exploracolombia
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material.icons.filled.Email
@@ -45,7 +47,8 @@ fun LoginScreen(
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(bottom = 24.dp),
+                .padding(bottom = 24.dp)
+                .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             // Header Image with Rounded Corners
