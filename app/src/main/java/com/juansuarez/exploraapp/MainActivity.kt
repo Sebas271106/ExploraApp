d377de862aebac81ceebbc89516d61d309ccc3e4
Sebas271106 <juansebastiansuarezpava@gmail.com>
2026-04-10 10:01:43 -0500

    Navegacion Login -> Register

--- app/src/main/java/com/juansuarez/exploraapp/MainActivity.kt
+++ app/src/main/java/com/juansuarez/exploraapp/MainActivity.kt
@@ -5,17 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.juansuarez.exploraapp.ui.theme.ExploraAppTheme
-import com.juansuarez.exploracolombia.LoginScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,10 +22,10 @@ class MainActivity : ComponentActivity() {
                 modifier = Modifier.fillMaxSize(),
             ) {
                 composable("login") {
-                    LoginScreen(onLoginSuccess = {}, onNavigateToRegister = {})
+                    LoginScreen(onLoginSuccess = {myNavController.navigate("register")}, onNavigateToRegister = { myNavController.navigate("register")})
                 }
                 composable("register") {
-                    RegisterScreen(onRegisterSuccess = {}, onNavigateToLogin = {})
+                    RegisterScreen(onRegisterSuccess = {myNavController.navigate("login")}, onNavigateToLogin = { myNavController.navigate("login")}, onBackClick = {myNavController.popBackStack()})
                 }
             }
         }
